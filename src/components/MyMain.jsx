fa70a16617c2b23f9790a6b9e95329300885114e
Teo <seminara.teodoro93@gmail.com>
2025-10-03 23:42:48 +0200

    css

--- src/components/MyMain.jsx
+++ src/components/MyMain.jsx
@@ -15,7 +15,9 @@ export default function MyMain() {
         e.preventDefault();
         axios.post("https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts", formData)
             .then(res => console.log("dati inviati", res.data))
-            .catch(error => console.log(error))
+            .catch(error => console.log(error));
+
+        alert("Modulo inviato correttamente")
     }
 
     const handleChange = (e) => {
@@ -28,30 +30,46 @@ export default function MyMain() {
         }
     }
 
+
+
+
+
     return (
         <>
-            <div>
-                <form onSubmit={handleSubmit}>
-                    <input type="text"
+
+            <form className="container" onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label for="exampleInputEmail1" className="form-label">Autore</label>
+                    <input className="form-control"
+                        type="text"
                         name="author"
                         value={formData.author} required
                         onChange={handleChange} />
-                    <input type="text"
+                </div>
+                <div className="mb-3">
+                    <label for="exampleInputPassword1" className="form-label">Titolo del libro</label>
+                    <input className="form-control"
                         name="title"
                         value={formData.title} required
                         onChange={handleChange} />
-                    <textarea
+                </div>
+                <div className="mb-3">
+                    <label for="exampleInputPassword1" className="form-label"></label>
+                    <textarea className="form-control"
                         name="body"
                         value={formData.body} required
-                        onChange={handleChange} ></textarea>
-                    <input type="checkbox"
+                        onChange={handleChange} />
+                </div>
+                <div className="mb-3 form-check">
+                    <input className="form-check-input"
+                        type="checkbox"
                         name="public"
                         value={formData.public}
                         onChange={handleChange} />
-                    <button type="submit">Invia richiesta</button>
-                </form>
-
-            </div>
+                    <label className="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" className="btn btn-primary">Invia richiesta</button>
+            </form>
         </>
     );
 };
